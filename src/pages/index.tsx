0b499d1adc953a9b0cdfb7cc92749f58357d712a
Rodrigo Santos <contato.rodrigosaantos@gmail.com>
2021-07-13 23:25:20 -0300

    Add form

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 import { Box, MainGrid, ProfileRelationsBoxWrapper } from '../styles/home';
-import { AlurakutMenu, OrkutNostalgicIconSet } from '../lib/AluraCommons';
+import { AlurakutMenu, AlurakutProfileSidebarMenuDefault, OrkutNostalgicIconSet } from '../lib/AluraCommons';
+import { FormEvent } from 'react';
 
 interface ProfileSidebarProps {
   userName: string;
@@ -9,6 +10,16 @@ function ProfileSidebar({ userName }: ProfileSidebarProps) {
   return (
     <Box>
       <img src={`https://github.com/${userName}.png`} style={{ borderRadius: '8px'}} />
+      <hr />
+
+      <p>
+        <a className='boxLink' href={`http://github.com/${userName}`}>
+          @{userName}
+        </a>
+      </p>
+
+      <hr />
+      <AlurakutProfileSidebarMenuDefault />
     </Box>
   )
 }
@@ -26,6 +37,14 @@ export default function Home() {
 
   ]
 
+  function handleSubmit(e: FormEvent) {
+
+    e.preventDefault();
+
+    console.log('Submit');
+
+  }
+
   return (
     <>
       <AlurakutMenu githubUser={githubUser} />
@@ -41,6 +60,30 @@ export default function Home() {
             </h1>
 
             <OrkutNostalgicIconSet />
+          </Box>
+
+          <Box>
+            <h2 className="subTitle">O que você deseja fazer</h2>
+            <form onSubmit={handleSubmit}>
+              <div>
+                <input 
+                  type="text"
+                  placeholder="Coloque uma URL para usarmos de capa"
+                  name="image"
+                  aria-label="Coloque uma URL para usarmos de capa"
+                />
+              </div>
+              <div>
+                <input 
+                  type="text"
+                  placeholder="Qual vai ser o name da sua comunidade?"
+                  name="title"
+                  aria-label="Qual vai ser o name da sua comunidade?"
+                />
+              </div>
+
+              <button>Criar comunidade</button>
+            </form>
           </Box>
         </div>
         <div className='profileRelationsArea' style={{ gridArea: 'profileRelationsArea'}}>
